0fc9f2f96171b0f866646347d656c28c4b6b281c
Kiri Um <kirirotha@gmail.com>
2020-07-28 13:22:23 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,13 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   belongs_to :network
+
+  def actors_list
+    actors = self.characters.map |c|
+      "#{c.first_name} #{c.last_name}"
+    end
+    actors
+  end
+
+
 end
