93336ab95cbd92ceafc231e99aef1a20d5070084
Kiri Um <kirirotha@gmail.com>
2020-07-28 12:27:43 +0000

    Automatically backed up by Learn

---
+++ db/migrate/005_add_name_and_id_to_character.rb
@@ -0,0 +1,10 @@
+class AddNameAndIdToCharacter < ActiveRecord::Migration[5.1]
+  #to make an additive change to a schema we create
+  #a new migration, and then in the change method,
+  #we make the change.
+  def change
+    add_column :character, :name, :string
+    add_column :character, :actor_id, :integer
+    add_column :character, :show_id, :integer
+  end
+end
